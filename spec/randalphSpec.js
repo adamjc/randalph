a4dbd7fd96524da493b2863ed25d008533591f3b
Adam Cook <Adam.Cook@bbc.co.uk>
2017-01-06 15:12:04 +0000

    Initial commit

---
+++ spec/randalphSpec.js
@@ -0,0 +1,21 @@
+const Randalph = require('../lib/randalph')
+
+describe('randalph', () => {
+  it('picks a random letter', () => {
+    const randalph = new Randalph()
+    const char = randalph.getChar()
+
+    expect(char).toMatch(/[A-Z]/)
+  })
+
+  it('picks the entire alphabet', () => {
+    const randalph = new Randalph()
+    let alphabet = []
+
+    for (var i = 0; i < 26; i += 1) {
+      alphabet.push(randalph.getChar())
+    }
+
+    expect(alphabet.sort().join('')).toEqual('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+  })
+})
---
+++ src/js/randalph.js
@@ -0,0 +1,19 @@
+function Randalph () {
+  let alphabet = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+  ]
+
+  function getChar () {
+    let randomIndex = Math.random() * alphabet.length
+
+    return alphabet.splice(randomIndex, 1)
+  }
+
+  return {
+    getChar: getChar
+  }
+}
+
+module.exports = Randalph
