46282709092c0838133b70c0e38e5aed995ada0f
adamjc <adamjc86@gmail.com>
2017-04-05 19:32:05 +0100

    Stop circle when we reach the end

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
+      radius: 110,
       char: randalph.getChar(),
       class: "randy animated"
     }
@@ -24,8 +25,7 @@ class App extends Component {
   handleInterval () {
     clearInterval(this.state.intervalId)
 
-    const circle = document.getElementById('circle')
-    const radius = circle.r.baseVal.value
+    const radius = this.state.radius
     const circumference = 2 * Math.PI * radius
     const timeLimit = 60 // in seconds
     const increment = circumference / timeLimit
@@ -35,7 +35,12 @@ class App extends Component {
 
     const intervalId = setInterval(_ => {
       const dT = ((new Date().getTime()) - lastTime) / 1000
-      circle.style['stroke-dasharray'] = `${currentLength} ${circumference}`
+
+      this.setState(_ => ({
+        circumference: circumference,
+        stroke: `${currentLength} ${circumference}`
+      }))
+
       currentLength += increment * dT
 
       if (currentLength >= circumference) {
@@ -51,12 +56,17 @@ class App extends Component {
   }
 
   handleClick () {
-    this.handleInterval()
     let char = randalph.getChar()
 
     if (char === '') {
+      clearInterval(this.state.intervalId)
+      this.setState(_ => ({
+        stroke: `0 ${this.state.circumference}`
+      }))
       char = <i className="fa fa-repeat" aria-hidden="true"></i>
       randalph.reset()
+    } else {
+      this.handleInterval()
     }
 
     this.setState(_ => ({
@@ -70,7 +80,7 @@ class App extends Component {
       <div className="App">
         <div className="container">
           <svg width="225" height="225">
-            <circle id="circle" transform="rotate(-90, 112.5, 112.5)" className="circle" cx="112.5" cy="112.5" r="110"/>
+            <circle id="circle" transform="rotate(-90, 112.5, 112.5)" className="circle" cx="112.5" cy="112.5" r={this.state.radius} style={{strokeDasharray: this.state.stroke}}/>
           </svg>
           <h1 onClick={this.handleClick} onAnimationEnd={this.handleTextAnim} className={this.state.class}>{this.state.char}</h1>
         </div>
